f534c5d26ad543f01f5e910bcde3ba6e676ed6bd
HPNeo <hpneo@hotmail.com>
2014-05-06 18:34:43 -0500

    Improve colors

--- assets/javascripts/charts.js
+++ assets/javascripts/charts.js
@@ -1,5 +1,32 @@
 var Charts = Charts || {};
-Charts.colors = ['#27ae60', '#2980b9', '#8e44ad', '#d35400', '#c0392b', '#16a085', '#f39c12'];
+// Charts.colors = [];
+Charts.colors = [
+  // '#499bca',
+  // '#6ec037',
+  // '#f1d131',
+  // '#ffa93a',
+  // '#ff2c22',
+  // '#6b2086'
+  '#367ca3',
+  '#7aaf3d',
+  '#e3b802',
+  '#ec9f2e',
+  '#ce222b',
+  '#55136b'
+  // '#275677',
+  // '#5a8a2c',
+  // '#c1951e',
+  // '#ca8026',
+  // '#a6121e',
+  // '#3a0049'
+  // '#27ae60',
+  // '#2980b9',
+  // '#8e44ad',
+  // '#d35400',
+  // '#c0392b',
+  // '#ffce00',
+  // '#f39c12'
+];
 
 Charts.donut_data = function(data) {
   var donutData = {
@@ -38,6 +65,8 @@ Charts.createDonut = function(options) {
     },
     plotOptions: {
       pie: {
+        borderColor: 'rgba(57, 23, 5, 0.4)',
+        borderWidth: 3,
         shadow: false,
         center: ['50%', '50%'],
         allowPointSelect: true,
