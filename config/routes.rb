ca63c294adef824757795c20175a89fa4a4bed12
HPNeo <hpneo@hotmail.com>
2014-06-06 18:19:07 -0500

    Update css and routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
-get 'dashboard', :to => 'report_dashboard#index'
-get 'dashboard/by_company', :to => 'report_dashboard#by_company'
-get 'dashboard/by_project', :to => 'report_dashboard#by_project'
+get '/dashboard', :to => 'report_dashboard#index'
+get '/dashboard/by_company', :to => 'report_dashboard#by_company'
+get '/dashboard/by_project', :to => 'report_dashboard#by_project'
 
 #get 'timelog_dashboard', :to => 'timelog_dashboard#index'
