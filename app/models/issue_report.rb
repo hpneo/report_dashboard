9f3ff718093310051b2d678057d5f35633648775
HPNeo <hpneo@hotmail.com>
2014-06-06 20:08:39 -0500

    Fix totals for IssueReport

--- app/models/issue_report.rb
+++ app/models/issue_report.rb
@@ -6,14 +6,12 @@ class IssueReport
     data = Issue.send(method, project) || []
 
     data.each do |datum|
-      datum["opened"] ||= datum["total"].to_i
-
       project_data[datum[id]] ||= {}
-      project_data[datum[id]]["opened"] ||= 0
-      project_data[datum[id]]["opened"] += datum["opened"].to_i
+      project_data[datum[id]]["total"] ||= 0
+      project_data[datum[id]]["total"] += datum["total"].to_i
       project_data[datum[id]]["closed"] ||= 0
       project_data[datum[id]]["closed"] += datum["closed"].to_i
-      project_data[datum[id]]["total"] = project_data[datum[id]]["opened"] + project_data[datum[id]]["closed"]
+      project_data[datum[id]]["opened"] = project_data[datum[id]]["total"] - project_data[datum[id]]["closed"]
     end
 
     project_data
@@ -28,13 +26,12 @@ class IssueReport
       project_data = Issue.send(method, project) || []
 
       project_data.each do |project_datum|
-        project_datum["opened"] ||= project_datum["total"]
-
         global_data[project_datum[id]] ||= {}
-        global_data[project_datum[id]]["opened"] ||= 0
-        global_data[project_datum[id]]["opened"] += project_datum["opened"].to_i
+        global_data[project_datum[id]]["total"] ||= 0
+        global_data[project_datum[id]]["total"] += project_datum["total"].to_i
         global_data[project_datum[id]]["closed"] ||= 0
         global_data[project_datum[id]]["closed"] += project_datum["closed"].to_i
+        global_data[project_datum[id]]["opened"] = global_data[project_datum[id]]["total"] - global_data[project_datum[id]]["closed"]
       end
     end
 
